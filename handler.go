02048941505610261947972959f9a4e6adff7681
Sascha Andres <sascha.andres@outlook.com>
2017-08-07 11:29:17 +0200

    Outlining process to setup and delete

--- handler.go
+++ handler.go
@@ -11,10 +11,21 @@ import (
 
 func (configuration *Configuration) SetupApplication(application string) error {
 	log.Println(fmt.Sprintf("Setup for %s", application))
+
+	// check and create app dir
+	// extract tar within a subdirectory YYYMMDDHHmmss
+	// delete symbolic link @ web/app
+	// create symolic link @ web/app pointing to app/YYYMMDDHHmmss
+	// remove directories that are not part of configured backup delay
+
 	return nil
 }
 
 func (configuration *Configuration) RemoveApplication(application string) error {
 	log.Println(fmt.Sprintf("Remove for %s", application))
+
+	// delete symbolic link @ web/app
+	// delete app dir
+
 	return nil
 }

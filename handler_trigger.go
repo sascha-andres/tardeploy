9d18fcd50372e2c37d817dfa4db72ba7edede39c
Sascha Andres <sascha.andres@outlook.com>
2017-08-08 18:45:21 +0200

    Running trigger

--- handler_trigger.go
+++ handler_trigger.go
@@ -1,6 +1,9 @@
 package tardeploy
 
 import (
+	"os"
+	"os/exec"
+
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
@@ -14,18 +17,36 @@ func (configuration *Configuration) afterRunTrigger(application string) error {
 }
 
 func (configuration *Configuration) trigger(application, status string) error {
-	log.Infof("Trigger for %s: %s", application, status)
+	var cmd string
 	switch status {
 	case "before":
 		if "" == configuration.Trigger.Before {
 			return nil
 		}
+		cmd = configuration.Trigger.Before
 		break
 	case "after":
 		if "" == configuration.Trigger.After {
 			return nil
 		}
+		cmd = configuration.Trigger.After
 		break
 	}
-	return errors.New("Trigger not yet implemented")
+
+	log.Infof("Trigger for %s: %s", application, status)
+
+	command := exec.Command(cmd, application, status)
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+	command.Stderr = os.Stderr
+	var err error
+	if err = command.Start(); err != nil {
+		return errors.Wrap(err, "could not start command")
+	}
+	err = command.Wait()
+	if err != nil {
+		return errors.Wrap(err, "Could not wait for command")
+	}
+
+	return nil
 }
